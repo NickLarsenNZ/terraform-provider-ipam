8bb2b31321ce6543d45d6cd6f956b0737cc81659
NickLarsenNZ <nick@aptiv.co.nz>
2020-03-26 23:21:12 +1300

    Initial dummy provider

---
+++ ipam/provider.go
@@ -0,0 +1,23 @@
+package ipam
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		//DataSourcesMap: map[string]*schema.Resource{
+		//	"template_file":             dataSourceFile(),
+		//	"template_cloudinit_config": dataSourceCloudinitConfig(),
+		//},
+		ResourcesMap: map[string]*schema.Resource{
+			"ipam_dummy": resourceDummy(),
+		},
+
+		// Later, specify a provider config schema
+		// Schema: nil,
+		// Later: pass in a configured API client
+		// ConfigureFunc: nil,
+	}
+}
